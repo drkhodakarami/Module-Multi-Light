02de1de2d18023c34593d7c0c42501bb0fd0413d
علیرضا خداکرمی <khodakarami_dr@outlook.com>
2023-03-02 21:57:53 +0330

    Add files via upload

---
+++ ModuleMultiLight/ModuleMultiLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleMultiLight : ModuleLight
+{
+    [KSPField]
+    public float lightRange = 60;
+
+    public override void OnStart(PartModule.StartState state)
+    {
+        base.OnStart(state);
+        lights = new List<Light>();
+        string[] temp = lightName.Replace(" ", "").Split(',');
+
+        foreach (var name in temp)
+            this.lights.AddRange(part.FindModelComponents<Light>(name));
+
+        foreach (var light in lights)
+        {
+            light.range = lightRange;
+            brightnessLevels.Add(light.intensity);
+            // Not sure if the reflection bellow will work or not!
+            light.color = (Color) typeof(ModuleLight).GetField("lightColor").GetValue(this);
+        }
+    }
+}
+
