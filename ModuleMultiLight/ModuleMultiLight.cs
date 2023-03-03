2ae7be337bad1851269a5542411ac31bf8e23f7a
علیرضا خداکرمی <khodakarami_dr@outlook.com>
2023-03-03 14:25:36 +0330

    Added Slider to editor for light range
    There is a known bug, when you turn on the lights on vessel, leave the vessel and come back, only the ring is colored, you need to turn the light off/on again so that every light would be active again
    
    Added the function to search under a single name for children as lights

--- ModuleMultiLight/ModuleMultiLight.cs
+++ ModuleMultiLight/ModuleMultiLight.cs
@@ -1,27 +1,79 @@
-﻿using System.Collections.Generic;
+using KSP.UI.Screens;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using Expansions.Missions.Adjusters;
 using UnityEngine;
 
 public class ModuleMultiLight : ModuleLight
 {
-    [KSPField]
+    [KSPField(guiActive = true, guiActiveEditor = true, guiName = "Light Range", isPersistant = true)]
+    [UI_FloatRange(maxValue = 300, minValue = 1, stepIncrement = 1f)]
     public float lightRange = 60;
+
+    public override void OnAwake()
+    {
+        base.OnAwake();
+        //TODO Fix the lights to turn them on when switching to vessel or starting flight again from space center
+        //GameEvents.onVesselLoaded.Add(_ => FixLightStates());
+        //GameEvents.onVesselChange.Add(_ => FixLightStates());
+        //GameEvents.onLaunch.Add(_ => FixLightStates());
+        Fields["lightRange"].OnValueModified += (_ => UpdateRange());
+    }
 
     public override void OnStart(PartModule.StartState state)
     {
         base.OnStart(state);
+        GetLightObjects();
+    }
+
+    private void GetLightObjects()
+    {
         lights = new List<Light>();
         string[] temp = lightName.Replace(" ", "").Split(',');
 
-        foreach (var name in temp)
-            this.lights.AddRange(part.FindModelComponents<Light>(name));
+        if (temp.Length < 1)
+            return;
 
+        if (temp.Length == 1)
+        {
+            var count = part.FindModelTransform(lightName).childCount;
+            if (count > 0)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    Transform obj = part.FindModelTransform(lightName).GetChild(i);
+                    string nam = obj.name;
+                    this.lights.AddRange(part.FindModelComponents<Light>(nam));
+                }
+            }
+        }
+        else
+        {
+            foreach (var name in temp)
+                this.lights.AddRange(part.FindModelComponents<Light>(name));
+        }
         foreach (var light in lights)
         {
             light.range = lightRange;
             brightnessLevels.Add(light.intensity);
-            // Not sure if the reflection bellow will work or not!
-            light.color = (Color) typeof(ModuleLight).GetField("lightColor").GetValue(this);
+            light.color = (Color)typeof(ModuleLight).GetField("lightColor").GetValue(this);
+        }
+        this.SetLightState(isOn, true);
+    }
+
+    public virtual void UpdateRange()
+    {
+        brightnessLevels.Clear();
+        foreach (var light in lights)
+        {
+            light.range = lightRange;
+            brightnessLevels.Add(light.intensity);
         }
     }
-}
 
+    public virtual void FixLightStates()
+    {
+        //TODO Fix the lights to turn them on when switching to vessel or starting flight again from space center
+    }
+}
